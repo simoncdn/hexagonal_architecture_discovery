0677770123b94e823daeba9e451a58b8e61f98ef
Simon Cardona <cdna.simon@gmail.com>
2024-08-25 21:27:33 +0200

    feat(page): Add page notifications

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -1,3 +1,17 @@
+import { Separator } from '@/components/ui/separator';
+
 export default function Notifications() {
-  return <div className={'flex-1 bg-orange-300'}>Notifications</div>;
+  return (
+    <div className={'flex-1 overflow-x-hidden overflow-y-scroll pr-4'}>
+      <div className="flex h-36 items-center justify-center">
+        <Separator className="w-60 bg-black" />
+        <span className="px-4 text-2xl font-bold">NOTIFICATIONS</span>
+        <Separator className="w-60 bg-black" />
+      </div>
+
+      <div className="flex justify-center">
+        <p>Aucune notification</p>
+      </div>
+    </div>
+  );
 }
